28cc724ef37b62a2ea66ee388ddb635b7878caad
unknown <ijlnflhbrzdev@gmail.com>
2022-09-12 09:04:07 +0700

    +[DONE] DICODING LOGIN PAGE

---
+++ android/app/src/main/kotlin/com/example/studikasus_dicoding_login_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studikasus_dicoding_login_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(17),
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
